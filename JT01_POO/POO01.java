956e10184cf2e13b780463146575d8e4572a5452
JOlmos98 <soler98@hotmail.es>
2023-11-13 12:16:47 +0100

    First commit

---
+++ JT01_POO/POO01.java
@@ -0,0 +1,9 @@
+package JT01_POO;
+
+public class POO01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Acabo de borrar los apuntes de Java de mi primer año dando Java, me quiero suicidar.\nPero bueno, aquí empezamos again.");
+	}
+}
